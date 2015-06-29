c053bf32b0ed1ba73d1f0139ecbf211f2ca5bd2e
Sy Le <lenguyensy@gmail.com>
2015-06-28 21:21:17 -0700

    fix install sublime text 3 to add aura events

--- install.sublime.sh
+++ install.sublime.sh
@@ -1,2 +1,3 @@
-#install on mac
-cp aura.sublime-completions ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/aura.sublime-completions
+#install on mac - sublime text 3
+cp aura.sublime-completions ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+cp aura.event.sublime-completions ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
