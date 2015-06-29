10c962294d2e788ced468c0396605c084acd880c
Sy Le <lenguyensy@gmail.com>
2015-06-28 20:30:43 -0700

    added dependencies for parsing xml

--- generateXmlAutoComplete.js
+++ generateXmlAutoComplete.js
@@ -22,15 +22,37 @@ var componentBaseDir = path.join(
 //find all cmp files in nested structures
 var componentFileNames = parseHelper.listDir(componentBaseDir);
 
-
+//generate xml stuffs
 componentFileNames.cmp.forEach(function(fileName){
-	console.log('cmp'.bold.blue, fileName.yellow);
-});
-
-componentFileNames.evt.forEach(function(fileName){
-	console.log('evt'.bold.blue, fileName.yellow);
+	// console.log('cmp'.bold.blue, fileName.yellow);
 });
 
 // var fileContent = parseHelper.readFromFile(utilJsPath);
 
 // /Users/syle/git/aura/aura-components/src/main/components/ui/updateGridRow/updateGridRow.evt
+
+
+//generate events stuffs
+componentFileNames.evt.forEach(function(fileName){
+	// console.log('evt'.bold.blue, fileName.yellow);
+	var fileContent = parseHelper.readFromFile(fileName);	
+	console.log(fileContent);
+});
+
+
+// var e = cmp.find('tabset2').get("e.addTab");
+
+//         e.setParams({tab: {
+//             "title": title,
+//             "closable": closable,
+//             "active": active,
+//             "body": [{
+//                 "componentDef": { descriptor:"markup://aura:text" },
+//                 "attributes": {
+//                     "values": {
+//                         "value": content
+//                     }
+//                 }
+//             }],
+//             }, index: -1});
+//         e.fire();
