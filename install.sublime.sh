cc6c38c6efd25f5551467f49b748e864fef92979
Sy Le <lenguyensy@gmail.com>
2015-07-07 09:43:36 -0700

    attempted at installing on ubuntu

--- install.sublime.sh
+++ install.sublime.sh
@@ -1,8 +1,33 @@
-#install on mac - sublime text 3
-echo "==="
-echo "Installing Aura Complete for Sublime Text 3"
-echo "Clean up previous installations of Aura"
-rm -f ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/aura*.sublime-completions
+#!/bin/bash
+if [ ! -d "~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User" ]
+then
+	# #install on mac - sublime text 3
+	echo "Directory Detected"
+	echo "~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User"
 
-echo "Copying aura snippets from bash"
-cp ./snippet/*.sublime-completions ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+	echo "==="
+	echo "Clean up previous installations of Aura"
+	rm -f ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/aura*.sublime-completions
+
+	echo "==="
+	echo "Copying aura snippets from bash"
+	cp ./snippet/*.sublime-completions ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+else
+	#!/bin/bash
+	if [ ! -d "~/config/sublime-text-3/Packages/User" ]
+	then
+		# #install on mac - sublime text 3
+		echo "Directory Detected"
+		echo "~/config/sublime-text-3/Packages/User"
+		
+		echo "==="
+		echo "Clean up previous installations of Aura"
+		rm -f ~/config/sublime-text-3/Packages/User/aura*.sublime-completions
+
+		echo "==="
+		echo "Copying aura snippets from bash"
+		cp ./snippet/*.sublime-completions ~/config/sublime-text-3/Packages/User
+	else
+		echo "Directory not found, please manually copy and paste the snippet to your Sublime Packages Folder."
+	fi
+fi
