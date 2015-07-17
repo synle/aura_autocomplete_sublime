08b628bc248b942e52a7c5500c24a0525d00f2d6
Sy Le <lenguyensy@gmail.com>
2015-07-16 21:27:17 -0700

    able to get the count out, attempt to draw the graph next

--- util/generateRelationship.js
+++ util/generateRelationship.js
@@ -15,53 +15,94 @@ module.exports = function processParser(componentFileNames, outputDir) {
         var fileBreakups = parseHelper.getComponentBreakup(fileName);
         var namespace = fileBreakups[0];
         var componentName = fileBreakups[1];
-        mappings[getFormattedName('app', namespace, componentName)] = {};
+        
+        var myFormattedName = getFormattedName('app', namespace, componentName);
+        // if(myFormattedName !== 'cmp.ui.datePicker'){
+        // 	return;
+        // }
+        mappings[myFormattedName] = {};
     });
     _.forEach(componentFileNames.cmp, function(fileName, idx) {
         var fileBreakups = parseHelper.getComponentBreakup(fileName);
         var namespace = fileBreakups[0];
         var componentName = fileBreakups[1];
-        mappings[getFormattedName('cmp', namespace, componentName)] = {};
+
+        var myFormattedName = getFormattedName('cmp', namespace, componentName);
+        // if(myFormattedName !== 'cmp.ui.datePicker'){
+        // 	return;
+        // }
+        mappings[myFormattedName] = {};
     });
-    _.forEach(componentFileNames.evt, function(fileName, idx) {
-        var fileBreakups = parseHelper.getComponentBreakup(fileName);
-        var namespace = fileBreakups[0];
-        var componentName = fileBreakups[1];
-        mappings[getFormattedName('evt', namespace, componentName)] = {};
-    });
-    var max = 10;//temporary return point
+    // _.forEach(componentFileNames.evt, function(fileName, idx) {
+    //     var fileBreakups = parseHelper.getComponentBreakup(fileName);
+    //     var namespace = fileBreakups[0];
+    //     var componentName = fileBreakups[1];
+    //     mappings[getFormattedName('evt', namespace, componentName)] = {};
+    // });
+
     //look at the files and find hints connecting the component
     componentFileNames.app.forEach(function(fileName) {
         var fileContent = parseHelper.readFromFile(fileName, true);
         var fileBreakups = parseHelper.getComponentBreakup(fileName);
         var namespace = fileBreakups[0];
         var componentName = fileBreakups[1];
+        
         //look for all components that used here
         parseComponentsUsed('app', fileName, namespace, componentName, fileContent);
-        max--;
-        if (max === 0) {
-            process.exit();
-        }
     });
     componentFileNames.cmp.forEach(function(fileName) {
         var fileContent = parseHelper.readFromFile(fileName, true);
+        var fileBreakups = parseHelper.getComponentBreakup(fileName);
+        var namespace = fileBreakups[0];
+        var componentName = fileBreakups[1];
+        
         //look for all components that used here
+        parseComponentsUsed('cmp', fileName, namespace, componentName, fileContent);
     });
 
+
+    logger.info(mappings)
+
+
+    //helpers
     function parseComponentsUsed(type, fileName, namespace, componentName, fileContent) {
-        console.log(namespace, componentName)
+        logger.debug(type.red.bold, namespace.red.bold, componentName.red.bold, fileName)
         parseString(fileContent, {
             async: true
         }, function(err, result) {
-            parseRecursiveComponent(result);
+            parseRecursiveComponent(type, namespace, componentName, result);
         });
     }
 
-    function parseRecursiveComponent(obj) {
+    function parseRecursiveComponent(type, namespace, componentName, obj) {
+    	var myFormattedName = getFormattedName(type, namespace, componentName);
+    	// logger.log(myFormattedName.red, obj);
+
         //should we directly change the mappings
+        for (var k in obj){
+        	if (k.indexOf(':') >= 0){
+        		//hooks it up
+	        	var curChildFormattedName = getFormattedNameFromString(k);
+
+	        	//increase count
+	        	if (_.isArray(obj[k])){
+	        		mappings[myFormattedName][curChildFormattedName] = obj[k].length;
+	        	}
+        		else{
+        			//parse further
+	        		mappings[myFormattedName][curChildFormattedName] = 1;
+        			parseRecursiveComponent(type, namespace, componentName, obj[k])
+        		}
+        	}
+        }
     }
 
     function getFormattedName(type, namespace, componentName) {
         return type + '.' + namespace + '.' + componentName;
+    }
+
+    function getFormattedNameFromString(str){
+    	var splits = str.split(':');
+    	return 'cmp.' + splits[0] + '.' + splits[1];
     }
 };
