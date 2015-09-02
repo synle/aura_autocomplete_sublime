0af464dc8f940af072b04752de38b48e60ae9e74
sle <sle@salesforce.com>
2015-09-01 20:07:15 -0700

    enhance mocking of navigator and window

--- util/generateJsAutoComplete.js
+++ util/generateJsAutoComplete.js
@@ -2,6 +2,7 @@
 var path = require('path');
 var colors = require('colors');
 var _ = require('lodash');
+var jsdom = require('jsdom')
 
 //internal dependencies
 var parseHelper = require('./parseHelper');
@@ -17,6 +18,9 @@ module.exports = function processParser(componentFileNames, outputDir){
 	var testJsPath;
 	var utilJsPath;
 
+	//master dictionary of util methjods
+	var masterDictionary = {};
+
 
 	_.forEach(componentFileNames.js, function(jsFileFullPath){
 		if(jsFileFullPath.indexOf('src/main/resources/aura/test/Test.js') >= 0){
@@ -26,7 +30,7 @@ module.exports = function processParser(componentFileNames, outputDir){
 			utilJsPath = jsFileFullPath;
 		}
 	});
-		
+
 
 	//needs to be defined
 	if (testJsPath === undefined || utilJsPath === undefined){
@@ -35,85 +39,90 @@ module.exports = function processParser(componentFileNames, outputDir){
 		logger.error('utilJsPath', utilJsPath);
 		return;
 	}
-	
 
 
-	//init Aura global obejct
-	var Aura = {
-		Utils: {
-			Util : {},
-			SecureFilters : {
-				html: ''
+
+	//properly mocking up required variables
+	jsdom.env({
+	  html: "<html><body></body></html>",
+	  done: function(errs, incomingWindow) {
+			//init Aura global obejct
+			var Aura = {
+				Utils: {
+					Util : {},
+					SecureFilters : {
+						html: ''
+					}
+				}
+			};
+
+			var $A = {
+				installOverride: function(){},
+				logger:{
+					subscribe:function(){}
+				}
+			};
+
+			var Component = AuraLayoutService = function(){};
+
+			//needs to be injected via code
+	    var window = incomingWindow;
+			var navigator = incomingWindow.navigator;
+
+
+			//start parsing
+			//AURA TEST JS FILE
+			//generarte path for the test and util js file
+			//read content files
+			var fileContent = parseHelper.readFromFile(
+				testJsPath,
+				true//silent
+			);
+
+
+			//parse test js
+			eval(fileContent);
+			var curNamespace = $A.test;
+			for (var k in $A.test){
+				var parsedStuffs = parseHelper.parseFunctions(k, curNamespace[k], 'A.test.');
+				if(parsedStuffs.length >0){
+					var functionName = parsedStuffs[0];
+					masterDictionary[functionName] = {
+						annotatedValue: parsedStuffs[1],
+						origValue : parsedStuffs[2]
+					};
+				}
 			}
-		}
-	};
-	
-	var AuraLayoutService = window = {};
-	var $A = {
-		installOverride: function(){},
-		logger:{
-			subscribe:function(){}
-		}
-	};
-	var Component = function(){};
-	var navigator = {
-		userAgent : ''
-	};
-
-
-	//master dictionary
-	var masterDictionary = {};
-
-	//AURA TEST JS FILE
-	//generarte path for the test and util js file
-	//read content files
-	var fileContent = parseHelper.readFromFile(
-		testJsPath,
-		true//silent
-	);
-
-
-	//parse test js
-	eval(fileContent);
-	var curNamespace = $A.test;
-	for (var k in $A.test){
-		var parsedStuffs = parseHelper.parseFunctions(k, curNamespace[k], 'A.test.');
-		if(parsedStuffs.length >0){
-			var functionName = parsedStuffs[0];
-			masterDictionary[functionName] = {
-				annotatedValue: parsedStuffs[1],
-				origValue : parsedStuffs[2]
-			};
-		}
-	}
 
 
 
-	//AURA UTILS JS FILE
-	//read content files
-	var fileContent = parseHelper.readFromFile(
-		utilJsPath,
-		true//silent
-	);
+			//AURA UTILS JS FILE
+			//read content files
+			var fileContent = parseHelper.readFromFile(
+				utilJsPath,
+				true//silent
+			);
 
 
-	//parse test js
-	eval(fileContent);
-	var curNamespace = Aura.Utils.Util.prototype;
-	for (var k in curNamespace){
-		var parsedStuffs = parseHelper.parseFunctions(k, curNamespace[k], 'A.util.');
-		if(parsedStuffs.length >0){
-			var functionName = parsedStuffs[0];
+			//parse test js
+			eval(fileContent);
+			var curNamespace = Aura.Utils.Util.prototype;
+			for (var k in curNamespace){
+				var parsedStuffs = parseHelper.parseFunctions(k, curNamespace[k], 'A.util.');
+				if(parsedStuffs.length >0){
+					var functionName = parsedStuffs[0];
 
-			masterDictionary[functionName] = {
-				annotatedValue: parsedStuffs[1],
-				origValue : parsedStuffs[2]
-			};
-		}
-	}
+					masterDictionary[functionName] = {
+						annotatedValue: parsedStuffs[1],
+						origValue : parsedStuffs[2]
+					};
+				}
+			}
 
 
 
-	//consolidate js file
-	parseHelper.updateJs(masterDictionary, outputDir);
+			//consolidate js file
+			parseHelper.updateJs(masterDictionary, outputDir);
+	  }
+	});
 }
